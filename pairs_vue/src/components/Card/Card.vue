8f0baf7e921cd47217e42a06ea768a6903475c13
porneczy <porneczy@gmail.com>
2023-04-07 20:42:39 +0200

    feature: add svg helper and style card

--- pairs_vue/src/components/Card/Card.vue
+++ pairs_vue/src/components/Card/Card.vue
@@ -1,19 +1,22 @@
-<script setup></script>
+<script setup>
+import icon from "../../helpers/svgHelper.vue";
+import { ref } from "vue";
+
+const isVisible = ref(false);
+
+const selectCard = () => {
+    isVisible.value = !isVisible.value;
+};
+</script>
 <template>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
-    <div class="card">Card</div>
+    <div class="card" @click="selectCard()">
+        <div v-if="!isVisible" class="front-side">
+            <h1>?</h1>
+        </div>
+        <div v-else class="back-side">
+            <icon name="card0" />
+        </div>
+    </div>
 </template>
 <style scoped lang="scss">
 @import "./Card.scss";

28a07037bca62ec2d24abbe2d6786d2b4d26cf13
porneczy <porneczy@gmail.com>
2023-04-08 14:04:15 +0200

    refarctor: cardlist, feature: add shuffle function

--- pairs_vue/src/App.vue
+++ pairs_vue/src/App.vue
@@ -5,23 +5,29 @@ import Card from "./components/Card/Card.vue";
 interface Card {
     icon: string;
     isVisible: boolean;
+    id: Number;
 }
 
-const cardList: Card[] = [];
+const NUMBER_OF_CARDS = 8;
 
-for (let i = 0; i <= 18; i++) {
-    cardList.push({
-        icon: `card${i}`,
-        isVisible: false,
-    });
+const cardList = Array.from({ length: NUMBER_OF_CARDS }, (_, i) => ({
+    icon: `card${i % (NUMBER_OF_CARDS / 2)}`,
+    isVisible: true,
+    id: i % (NUMBER_OF_CARDS / 2),
+}));
+
+function shuffleCards(cards: Card[]): Card[] {
+    return cards.sort(() => Math.random() - 0.5);
 }
+
+const shuffledCardList = shuffleCards(cardList);
 </script>
 
 <template>
     <AppHeader />
     <div class="game-board-container">
         <Card
-            v-for="(card, index) in cardList"
+            v-for="(card, index) in shuffledCardList"
             :isVisible="card.isVisible"
             :icon="card.icon"
             :key="index"
